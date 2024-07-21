aead8516517929ef23dbd4fb5f81cb81f20ef53b
Harshit0523 <harshupahade2004@gmail.com>
2024-07-21 20:28:34 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return <main className='text-5xl  p-2 '>
+    <section className='xl:padding-1'> 
+      hero
+    </section>
+  </main>
+}
+
+export default App
